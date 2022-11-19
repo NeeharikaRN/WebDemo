dfeba61792e26c17c66cbda12925381e79a92014
NeeharikaRN <n.neeharikareddy@gmail.com>
2022-11-19 15:44:56 +0530

    Update WebDemoApplication.java

--- src/main/java/com/example/WebDemo/WebDemoApplication.java
+++ src/main/java/com/example/WebDemo/WebDemoApplication.java
@@ -9,5 +9,18 @@ public class WebDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebDemoApplication.class, args);
 	}
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
 
 }
